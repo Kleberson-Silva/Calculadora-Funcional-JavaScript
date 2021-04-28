8798626a704373d9afcbd43176312a3a7f8c3186
Kleberson-Silva <silva.kleberson@gmail.com>
2021-04-28 11:17:15 -0300

    :package: funcionalidade de copiar e colar

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -13,10 +13,33 @@ class CalcController{
          this.initButtonsEvents();
          this.initKeyboard();
      }
-     
+     pasteFromClipboard(){
 
+         document.addEventListener('paste', e=>{
 
-     
+            let text = e.clipboardData.getData('Text');
+
+            this.displayCalc = parseFloat(text);
+
+         });
+
+     }
+
+     copyToClipboard(){
+
+         let input = document.createElement('input');
+
+         input.value = this.displayCalc;
+
+         document.body.appendChild(input);
+
+         input.select();
+
+         document.execCommand("Copy");
+
+         input.remove();
+
+     }
      initialize(){
 
 
@@ -30,6 +53,7 @@ class CalcController{
             },1000);
 
             this.setLastNumberToDisplay();
+            this.pasteFromClipboard();
 
 
      }
@@ -75,6 +99,11 @@ class CalcController{
                case '9':
                   this.addOperation(parseInt(e.key));
                   break;
+
+               case 'c': 
+               if(e.ctrlKey) this.copyToClipboard();
+
+               break;
    
             }
             
